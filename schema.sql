537a5e830aa6ef5bd58eab494b61d972107f7d35
Cody Dawkins <dawkinscody4@gmail.com>
2023-02-09 11:50:53 -0500

    Create SQL db tables

--- schema.sql
+++ schema.sql
@@ -1,2 +1,33 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name DECIMAL(30),
+  role_id INT FOREIGN KEY,
+  manager_id INT FOREIGN KEY,
+  FOREIGN KEY (role_id) REFERENCES role(id)
+  FOREIGN KEY (employee_id) REFERENCES employee(id)
+
+);
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30),
+
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT FOREIGN KEY,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
 
 
